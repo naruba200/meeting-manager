120a39e64f655b54da469cf26f1651f28da8c679
nguyen1405 <hoangxuannguyen1234@gmail.com>
2025-09-25 09:20:58 +0700

    aaaa

--- meeting-manager/src/routes/routes.jsx
+++ meeting-manager/src/routes/routes.jsx
@@ -1,13 +1,15 @@
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
-
+import UserList from "../pages/UserList";
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<UserList />} />
+        <Route path="/user-list" element={<UserList />} />
         {/* sau này thêm /register, /dashboard */}
       </Routes>
     </BrowserRouter>
